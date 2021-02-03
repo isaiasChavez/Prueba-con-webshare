1ba7c67e7303d6fa65e504a189733edff6bbfb44
Isaias Chavez Martinez <isaiaschavez.co@gmail.com>
2021-02-03 15:07:18 -0600

    .

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
-import logo from './logo.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
-function App () {
-  const files = ['https://expobebe.fra1.digitaloceanspaces.com/macrotextura_1280.png']
+function App() {
 
   const shareData = {
-    files,
-    title: 'Esta es una prueba de compartir',
-    text: 'Compartemeeeee'
-  }
+  title: 'Holaaa',
+  text: 'Compartemeeee!',
+  url: 'https://expobebe.fra1.digitaloceanspaces.com/macrotextura_1280.png',
+}
 
-  const compartir = async () => {
-    try
-    {
 
-      if( navigator.canShare )
-      {
-        navigator
-          .share(shareData)
-          .then(() => console.log('Share was successful.'))
-          .catch(error => console.log('Sharing failed', error))
-      } else {
-        alert(`Your system doesn't support sharing files.`)
-      }
+  const compartir = async () =>{
+    try {
+      await navigator.share(shareData)
+      
     } catch (error) {
-      alert('Error',error.message)
-      console.log(error)
+      alert("Error",error.message)
+      console.log(error);
     }
   }
 
+
   return (
-    <div className='App'>
+    <div className="App">
+
       <div>
-        <h1>Comparte esta foto</h1>
-        <img src='/logo192.png' alt='asdf' />
+        <h1>Comparte esta foto</h1>    
+        <img src="/logo192.png" alt="asdf"/>
       </div>
-      <button onClick={compartir} class='btn btn-primary btn-lg'>
-        Compartir
-      </button>
+      <button onClick={compartir} class="btn btn-primary btn-lg">Compartir</button>
+    
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
