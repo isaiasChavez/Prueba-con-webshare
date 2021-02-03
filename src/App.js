b99d36afee3322a3a4d76db65d5d8ce0683b7d26
Isaias Chavez Martinez <isaiaschavez.co@gmail.com>
2021-02-03 14:26:00 -0600

    second

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const shareData = {
+  title: 'MDN',
+  text: 'Learn web development on MDN!',
+  url: 'https://developer.mozilla.org',
+}
+
+
+  const compartir = async () =>{
+    try {
+      await navigator.share(shareData)
+      
+    } catch (error) {
+      alert("Error")
+      console.log(error);
+    }
+  }
+
+
   return (
     <div className="App">
 
@@ -9,7 +28,7 @@ function App() {
         <h1>Comparte esta foto</h1>    
         <img src="/logo192.png" alt="asdf"/>
       </div>
-      <button class="btn btn-primary btn-lg">Compartir</button>
+      <button onClick={compartir} class="btn btn-primary btn-lg">Compartir</button>
     
     </div>
   );
